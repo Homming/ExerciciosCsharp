cb9a7c16664cec7b140be893f959f7e4691414e5
Nicholas Torres <nicolas_br11@hotmail.com>
2021-09-05 22:46:13 -0300

    Exercicior 2 de orientação a objeto em c#

---
+++ Exercicio 2 Orientação a Objeto/Exercicio 2 Orientação a Objeto/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio_2_Orientação_a_Objeto {
+    class Funcionario {
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public double salarioLiquido() {
+            return salarioBruto - imposto;
+        }
+
+        public void aumentarSalario(double porcentagem) {
+            salarioBruto += salarioBruto * (porcentagem / 100);
+        }
+    }
+}
---
+++ Exercicio 2 Orientação a Objeto/Exercicio 2 Orientação a Objeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_2_Orientação_a_Objeto {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario fulano = new Funcionario();
+
+            Console.WriteLine("Digite o nome do Funcionário");
+            fulano.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o Salario do Funcionário");
+            fulano.salarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o imposto a ser taxado do Funcionário");
+            fulano.imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Funcionário: {fulano.nome}, {fulano.salarioLiquido()}");
+
+            Console.Write("Digite a porcentagem para aumentar sálario: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados atualizados: {fulano.nome}, {fulano.salarioLiquido()}");
+        }
+    }
+}
